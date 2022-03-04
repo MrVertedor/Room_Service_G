0715afe18b8fa9774b8ed6679856f85464832c59
MrVertedor <98377354+MrVertedor@users.noreply.github.com>
2022-03-04 13:53:16 +0800

    First code

--- room_service.py
+++ room_service.py
@@ -3,4 +3,21 @@ This program is ...
 Created by ...
 """
 
-#Intro
+guests = {'102':'Peter', '103':'Peter', '105':'Mary', '107':'Daniel'}
+
+def check_guest(name, room):
+    msg = None
+    if room in guests.keys():
+        if guests[room] == name:
+            msg = 'Welcome!'
+        else:
+            msg = "Your info doesn't match out records"
+    else:
+        msg = f"Room {room} hasn't been booked."
+    return msg
+
+
+name = input("What's your name? ")
+room = input("What's your room number? ")
+
+print (check_guest(name, room))
